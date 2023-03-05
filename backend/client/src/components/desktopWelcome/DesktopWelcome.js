94a4c8a9a6d1caa39f7a342b532bec5728b7e6b1
Marius <zebrauskas.mar@gmail.com>
2023-03-05 11:45:11 +0000

    welcome changess

--- backend/client/src/components/desktopWelcome/DesktopWelcome.js
+++ backend/client/src/components/desktopWelcome/DesktopWelcome.js
@@ -1,34 +1,45 @@
-import React from 'react';
-import { Wrapper, WrapperH1, H1, WrapperText, P } from './stylesDesktopWelcome';
+import React from "react";
+import { Wrapper, WrapperH1, H1, WrapperText, P } from "./stylesDesktopWelcome";
 
 const animationOut = {
   hiden: {
     opacity: 1,
-    x: '0rem',
+    x: "0rem",
   },
   animate: {
     opacity: 0,
-    x: '-10rem',
-    transition: { duration: 0.8, ease: 'easeInOut', delay: 0 },
+    x: "-10rem",
+    transition: { duration: 0.8, ease: "easeInOut", delay: 0 },
   },
 };
 const animationIn = {
   hiden: {
     opacity: 0,
-    x: '-10rem',
+    x: "-10rem",
   },
   animate: {
     opacity: 1,
-    x: '0rem',
-    transition: { duration: 0.8, ease: 'easeInOut', type: 'spring', delay: 0.5 },
+    x: "0rem",
+    transition: {
+      duration: 0.8,
+      ease: "easeInOut",
+      type: "spring",
+      delay: 0.5,
+    },
   },
 };
 const DesktopWelcome = ({ openWelcome }) => {
   return (
     <Wrapper
-      variants={openWelcome === 'out' ? animationIn : openWelcome === 'in' ? animationOut : null}
-      initial='hiden'
-      animate='animate'
+      variants={
+        openWelcome === "out"
+          ? animationIn
+          : openWelcome === "in"
+          ? animationOut
+          : null
+      }
+      initial="hiden"
+      animate="animate"
     >
       <WrapperH1>
         <H1>Marius</H1>
@@ -36,8 +47,8 @@ const DesktopWelcome = ({ openWelcome }) => {
       </WrapperH1>
       <WrapperText>
         <P>
-          I'm a REACT JS Web Developer. I enjoy writing functions and building beautiful responsive
-          websites.
+          I'm a Javascript Web Developer. I enjoy writing functions and building
+          beautiful responsive websites.
         </P>
       </WrapperText>
     </Wrapper>
