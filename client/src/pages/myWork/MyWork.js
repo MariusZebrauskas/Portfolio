af0fa120f39d0ecc0a499ddeed674e9f390012db
Marius Zebrauskas <zebrauskas.mar@gmail.com>
2024-10-22 21:34:06 +0300

    update job list

--- client/src/pages/myWork/MyWork.js
+++ client/src/pages/myWork/MyWork.js
@@ -13,6 +13,48 @@ import {
   Header,
   TodoImage,
 } from "./stylesMyWork";
+
+const projects = [
+  {
+    id: Math.random(),
+    title: "Embrk Limited (working since 09-2022)",
+    imgClass: "test",
+    imgComponent: <TodoImage className="musicschoolnotes test" />,
+    sliderData: {
+      data1: "test",
+      data2: "test",
+      data3: "test",
+    },
+    description: "test",
+  },
+  {
+    id: Math.random(),
+    title: "Full Stack Application",
+    imgClass: "blurOlgos",
+    imgComponent: <OlgosWebImg className="musicschoolnotes blurOlgos" />,
+    sliderData: {
+      data1: "Login: testmyweb@gmail.com",
+      data2: "Password: helloworld",
+      data3: "musicschoolnotes",
+    },
+    description:
+      "Technologies: React, Styled Components, Framer Motion, React Router, Context Api, Node JS, Express, Mongo DB, Fire Base, Stripe, Node Mailer...",
+  },
+  {
+    id: Math.random(),
+    title: "Full Stack Application Nr. 2",
+    imgClass: "blurSmartBook",
+    imgComponent: <TodoImage className="musicschoolnotes blurSmartBook" />,
+    sliderData: {
+      data1: "Login: testmyweb@gmail.com",
+      data2: "Password: helloworld",
+      data3: "smartbook",
+    },
+    description:
+      "Technologies: NextJS, TypesScript, Redux, Tailwind, GSAP, Mongo DB, Fire Base, Node Mailer, Bcrypt, Joi, Axios.",
+  },
+];
+
 const MyWork = ({ setOpenWelcome, setAudioSound }) => {
   useEffect(() => {
     // scroll up on render
@@ -22,8 +64,8 @@ const MyWork = ({ setOpenWelcome, setAudioSound }) => {
     return () => {};
   }, []);
 
-  const slider = useRef();
-  const slider2 = useRef();
+  // const slider = useRef();
+  // const slider2 = useRef();
 
   // animation on load
   useEffect(() => {
@@ -46,7 +88,29 @@ const MyWork = ({ setOpenWelcome, setAudioSound }) => {
   // timer runs 1 animation at the time
   let timer = null;
 
+  return (
+    <Wrapper>
+      <MyWorkH1 className="animate">my work</MyWorkH1>
+      <WrapperCompleateProjects>
+        {projects.map((project) => (
+          <Project
+            project={project}
+            key={project.id}
+            setAudioSound={setAudioSound}
+          />
+        ))}
+      </WrapperCompleateProjects>
+    </Wrapper>
+  );
+};
+
+export default MyWork;
+
+let timer = null;
+function Project({ project, setAudioSound }) {
   const onMouseEnter = (param1, param2) => {
+    let timer = null;
+
     // timer stops overlaping animation wich brokeanimation
     // timer runs 1 animation at the time
     if (timer === param1) {
@@ -60,6 +124,9 @@ const MyWork = ({ setOpenWelcome, setAudioSound }) => {
     });
     gsap.to(param1, { duration: 0.2, filter: "blur(5px)" });
   };
+
+  let timer = null;
+
   // blure background
   const onMouseLeave = (param1, param2) => {
     // timer stops overlaping animation wich brokeanimation
@@ -81,61 +148,27 @@ const MyWork = ({ setOpenWelcome, setAudioSound }) => {
     }, 601);
   };
 
+  const slider = useRef();
+
   return (
-    <Wrapper>
-      <MyWorkH1 className="animate">my work</MyWorkH1>
-      <WrapperCompleateProjects>
-        {/* jobe1 */}
-        <WrapperEachJobe className="animate">
-          <Header className="animate">Full Stack Application </Header>
-          <ImgWrapper
-            onMouseEnter={() => onMouseEnter(".blurOlgos", slider)}
-            onMouseLeave={() => onMouseLeave(".blurOlgos", slider)}
-            ref={slider}
-          >
-            <OlgosWebImg className="musicschoolnotes blurOlgos" />
-            <Slider
-              setAudioSound={setAudioSound}
-              data1="Login: testmyweb@gmail.com"
-              data2="Password: helloworld"
-              data3="musicschoolnotes"
-            />
-          </ImgWrapper>
-          <DescriotionWrapper>
-            <P className="animate">
-              Technologies: React, Styled Components, Framer Motion, React
-              Router, Context Api, Node JS, Express, Mongo DB, Fire Base,
-              Stripe, Node Mailer...
-            </P>
-          </DescriotionWrapper>
-        </WrapperEachJobe>
-        {/* jobe2 */}
-
-        <WrapperEachJobe className="animate">
-          <Header className="animate">Full Stack Application Nr. 2</Header>
-          <ImgWrapper
-            onMouseEnter={() => onMouseEnter(".blurSmartBook", slider2)}
-            onMouseLeave={() => onMouseLeave(".blurSmartBook", slider2)}
-            ref={slider2}
-          >
-            <TodoImage className="musicschoolnotes blurSmartBook" />
-            <Slider
-              setAudioSound={setAudioSound}
-              data1="Login: testmyweb@gmail.com"
-              data2="Password: helloworld"
-              data3="smartbook"
-            />
-          </ImgWrapper>
-          <DescriotionWrapper>
-            <P className="animate">
-              Technologies: NextJS, TypesScript, Redux, Tailwind, GSAP, Mongo
-              DB, Fire Base, Node Mailer, Bcrypt, Joi, Axios.
-            </P>
-          </DescriotionWrapper>
-        </WrapperEachJobe>
-      </WrapperCompleateProjects>
-    </Wrapper>
+    <WrapperEachJobe key={project.id} className="animate">
+      <Header className="animate">{project.title}</Header>
+      <ImgWrapper
+        onMouseEnter={() => onMouseEnter(`.${project.imgClass}`, slider)}
+        onMouseLeave={() => onMouseLeave(`.${project.imgClass}`, slider)}
+        ref={slider}
+      >
+        {project.imgComponent}
+        <Slider
+          setAudioSound={setAudioSound}
+          data1={project.sliderData.data1}
+          data2={project.sliderData.data2}
+          data3={project.sliderData.data3}
+        />
+      </ImgWrapper>
+      <DescriotionWrapper>
+        <P className="animate">{project.description}</P>
+      </DescriotionWrapper>
+    </WrapperEachJobe>
   );
-};
-
-export default MyWork;
+}
